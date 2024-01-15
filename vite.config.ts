72a945d0414640d2d7954dd18fb19aec6c73a689
Garrett Keneau <gart@gart.sh>
2024-01-15 16:42:37 -0500

    fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     root: './src',
+    base: '/rgb',
     build: {
         outDir: '../dist',
         emptyOutDir: true,
-    }
+    },
 })
